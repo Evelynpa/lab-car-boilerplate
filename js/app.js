d08a302dfb7cbf560890b298314430c64916f67c
Evelynpa <evelyn.parra.rojas@gmail.com>
2017-12-06 11:30:21 -0300

    se crea pagina responsive con framewors y modal

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 // La funcionalidad de tu proyecto
+  $(document).ready(function(){
+
+   $('#inicio').click(function(){
+		$('#modal1').modal();
+
+   });
+   
+    $('#registr').click(function(){
+		$('#modal2').modal();
+
+   });
+    $('#drive').click(function(){
+		$('#modal3').modal();
+
+   });
+  });
